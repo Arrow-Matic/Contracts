9a7c542693d6e0fa3c070b01386069b6ccbb9195
akashjha011 <akash.jsmith@gmail.com>
2021-07-06 09:44:13 +0530

    Contract update

--- PoERewards.sol
+++ PoERewards.sol
@@ -2,39 +2,99 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "./EIP712MetaTransaction.sol";
 
-contract PoERewards is ERC721URIStorage,EIP712MetaTransaction("PoERewards","1") {
+contract PoERewards is ERC721URIStorage {
     
     uint256 private tokenId;
-    address private _owner;
+    uint256 private bountyId;
     
     
+    //mapping of bounty info to bounty ID 
+    mapping(uint256 => NFTbounties) bounties;
+    
+    //mapping to check reward claim status by user
+    mapping(address => mapping(uint256=>bool)) userRewards;
     
     struct user{
-        address userAddress;
         uint256 userPoints;
+        address userAddress;
     }
     
-    user[10] public Leaderboard;
+    struct NFTbounties{
+        address contractAddress;
+        string URI;
+        uint256 distributionNumber;
+        uint256 distributedTo;
+    }
+    
+    uint256 private BountyCreationCost;
+    user[10] private Leaderboard;
+    
+    
     
     event Claimed(address indexed _by, uint256 indexed _tokenId);
     event LeaderBoardUpdate(user[10] Leaderboard);
-
-    
+    event BountyCreated(address indexed _contract,string URI,uint256 quantity);
     
     constructor() ERC721("Proof Of Experience","POE"){
         tokenId = 1;
+        bountyId = 1;
+        BountyCreationCost = 0.05 *1 ether;
         _owner = msg.sender;
     }
     
-    function Claim(string memory tokenURI,uint256 points) external{
-        _safeMint(msgSender(), tokenId);
-        _setTokenURI(tokenId, tokenURI);
-        emit Claimed(msgSender(), tokenId);
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+    
+    function BountyExists(uint256 bountyID) internal view returns(bool){
+        if(bounties[bountyID].contractAddress != address(0))
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    function CreateBounty(address contractAdd,string memory URI, uint256 quantity) external payable{
+        require(msg.value == BountyCreationCost,"POE : Sent value is not the price");
+        NFTbounties memory bounty;
+        bounty.contractAddress = contractAdd;
+        bounty.URI = URI;
+        bounty.distributionNumber = quantity;
+        bounties[bountyId] = bounty;
+        emit BountyCreated(contractAdd,URI,quantity);
+    }
+    
+    function ClaimBounty(uint256 bountyID,uint256 points) external{
+        require(BountyExists(bountyID),"POE : Bounty doesn't exist");
+        require(!userRewards[msg.sender][bountyID],"POE: Bounty claimed already");
+        userRewards[msg.sender][bountyID] = true;
+        _safeMint(msg.sender, tokenId);
+        _setTokenURI(tokenId, bounties[bountyID].URI);
+        emit Claimed(msg.sender, tokenId);
         tokenId = tokenId + 1;
         checkLeaderBoard(points);
-        
     }
     
     function checkLeaderBoard(uint256 points) internal returns(bool){
@@ -47,7 +107,7 @@ contract PoERewards is ERC721URIStorage,EIP712MetaTransaction("PoERewards","1") 
         address tempAdd;
         
         Leaderboard[9].userPoints = points;
-        Leaderboard[9].userAddress = msgSender();
+        Leaderboard[9].userAddress = msg.sender;
         
         for(i=8;i>=0;i--)
         {
@@ -66,8 +126,14 @@ contract PoERewards is ERC721URIStorage,EIP712MetaTransaction("PoERewards","1") 
             }
         }
         emit LeaderBoardUpdate(Leaderboard);
+        return true;
     }
     
-
+    function getLeaderBoard() external view returns(user[10] memory){
+        return Leaderboard;
+    }
     
+    function retrieve() external onlyOwner {
+        payable(owner()).transfer(balanceOf(address(this)));
+    }
 }
